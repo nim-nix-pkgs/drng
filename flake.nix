9ffa6413ae1c67c3832f37c192b3902079bc221a
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:09 -0300

    chore: re index drng

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."drng-main".dir   = "main";
+  inputs."drng-main".owner = "nim-nix-pkgs";
+  inputs."drng-main".ref   = "master";
+  inputs."drng-main".repo  = "drng";
+  inputs."drng-main".type  = "github";
+  inputs."drng-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."drng-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
